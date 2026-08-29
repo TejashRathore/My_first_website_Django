5dc277d84019a32fc98af3ce4c7f1441f2f1cff6
Tejash Rathore <tr4625078@gmail.com>
2026-08-30 00:28:35 +0530

    Refactor blog templates and views for improved structure, make it more dynamic and readability

--- python/MyDjangoProjects/mywebsite/blogs/views.py
+++ python/MyDjangoProjects/mywebsite/blogs/views.py
@@ -1,38 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
-from django.template.loader import render_to_string
 blog_names = {
-    "python-intro":"<h1>Hello user's, You're at the python_intro page.</h1>",
-    "django-intro": "<h1>Hello user's, You're at the django-intro page.</h1>",
-    "regex":"<h1> Hello user's, you're at the regex intro page\nit is use for the file handling in the python.</h1>",
-    "python-oops": "<h1> Hello user's you're at the python oops(object oriented programming)-intro page.</h1>",
+    "python-intro":"Hello user's, You're at the python_intro page.",
+    "django-intro": "Hello user's, You're at the django-intro page.",
+    "regex":" Hello user's, you're at the regex intro page\nit is use for the file handling in the python.",
+    "python-oops": " Hello user's you're at the python oops(object oriented programming)-intro page.",
 
 }
 
 # Create your views here.
 
 def home(request):
-    res_html=render_to_string("blog/index.html")
-    return HttpResponse(res_html)
+    return render(request,"blog/index.html")
+   # res_html=render_to_string("blog/index.html")
+   # return HttpResponse(res_html)
 
 def blogposts(request):
+    # Initialize an empty string to accumulate HTML list items
     list_items = ""
+    
+    # Extract all blog names (keys) from the blog_names dictionary as a list
     blog_keys = list(blog_names.keys())
+    
+    # Iterate through each blog name to generate dynamic links
     for b in blog_keys:
+        # Generate the URL path for each blog post using Django's reverse() function with the blog name as an argument
         blog_path = reverse("blog-posts", args=[b])
+        
+        # Construct an HTML list item with a hyperlink, capitalizing the blog name for display
         list_items += f'<li><a href = "{blog_path}">{b.capitalize()}</a></li>'
+    
+    # Wrap all list items in an unordered list (<ul>) HTML tag
     res_data=f'<ul>{list_items}</ul>'
     return HttpResponse(res_data)
 
 
+def process_blog_name(blog):
+    blog_list = blog.split("-")
+    return " ".join(blog_list).title()
+# To make the templates heading more proffesional, python-intro => Python Intro
+
 def blog_posts(request, blog):
     try:
         res = blog_names[blog]
+        return render(request, "blog/posts.html", 
+    {"blog_text":res, "blog_name":process_blog_name(blog)})# this is the way we can make more dynamic templates by using DTL.
     except Exception:
         return HttpResponseNotFound("<h1>Blog post not found.</h1>")
-    else:
-         return HttpResponse(res)
+   
 
 
 def blogposts_by_number(request, blog):
