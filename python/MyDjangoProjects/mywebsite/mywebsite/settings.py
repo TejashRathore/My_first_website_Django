d4feecb0b3633ca1bf2cc10d052b3db139516733
Tejash Rathore <tr4625078@gmail.com>
2026-09-04 14:42:15 +0530

    Update TEMPLATES setting to include base template directory and add base.html template

--- python/MyDjangoProjects/mywebsite/mywebsite/settings.py
+++ python/MyDjangoProjects/mywebsite/mywebsite/settings.py
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR/'blogs'/'templates'
+            BASE_DIR/"templates"
+            
         ],
         'APP_DIRS': True,
         'OPTIONS': {
